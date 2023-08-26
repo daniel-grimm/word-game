d01f9e676bc6ea005a0a132c8c4e28746e90552a
Daniel Grimm <danielsgrimm@hotmail.com>
2023-08-25 23:32:56 -0500

    Correcting CSS Value
    Changing from plural to singular

--- word-game/src/components/Answer/Answer.style.tsx
+++ word-game/src/components/Answer/Answer.style.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Box = styled.div<{ color: string }>`
     display: flex;
     align-items: center;
-    justify-contents: center;
+    justify-content: center;
     height: 30px;
     width: 30px;
     background-color: ${(props) => props.color};
